60d60f27a3094893981aae77684b97f5d84dfc79
Ken Lloyd <mail.com>
2021-10-04 10:55:17 +0800

    added function in button

--- app/src/main/java/com/example/umonlineappointmentsystem/MainActivity.java
+++ app/src/main/java/com/example/umonlineappointmentsystem/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.umonlineappointmentsystem;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(view -> {
+            Intent i = new Intent(this, DashboardActivity.class);
+            startActivity(i);
+        });
     }
 }
