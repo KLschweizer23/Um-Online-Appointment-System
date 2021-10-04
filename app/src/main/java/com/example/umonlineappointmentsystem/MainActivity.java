c57ecf27eba9364dc5dda7024389b2a937901ff7
Ken Lloyd <mail.com>
2021-10-04 19:37:56 +0800

    finished animation spash screen

--- app/src/main/java/com/example/umonlineappointmentsystem/MainActivity.java
+++ app/src/main/java/com/example/umonlineappointmentsystem/MainActivity.java
@@ -2,9 +2,12 @@ package com.example.umonlineappointmentsystem;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ActivityOptions;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.util.Pair;
+import android.view.View;
 import android.view.WindowManager;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
@@ -14,20 +17,36 @@ import android.widget.ImageView;
 public class MainActivity extends AppCompatActivity {
 
     //Variables
-    Animation toTopAnim, resizeAnim;
-    ImageView iv_logo, iv_back;
+    Animation topAnim, bottomAnim;
+    ImageView iv_logo, iv_title;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        topAnim = AnimationUtils.loadAnimation(this, R.anim.up_animation);
+        bottomAnim = AnimationUtils.loadAnimation(this, R.anim.down_animation);
+
+        iv_logo = findViewById(R.id.um_logo2);
+        iv_title = findViewById(R.id.title);
+
+        iv_logo.setAnimation(topAnim);
+        iv_title.setAnimation(bottomAnim);
+
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
                 Intent i = new Intent(MainActivity.this, LoginActivity.class);
-                startActivity(i);
+
+                Pair[] pairs = new Pair[2];
+                pairs[0] = new Pair<View, String>(iv_logo, "um_logo");
+                pairs[1] = new Pair<View, String>(iv_title, "um_title");
+
+                ActivityOptions options = ActivityOptions.makeSceneTransitionAnimation(MainActivity.this, pairs);
+                startActivity(i, options.toBundle());
                 finish();
+
             }
         },5000);
     }
