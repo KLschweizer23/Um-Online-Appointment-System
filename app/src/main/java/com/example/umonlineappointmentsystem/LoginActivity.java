80b699c75500b8ebdfd1b463b6b06396b9b02083
Ken Lloyd <mail.com>
2021-10-06 06:35:30 +0800

    finished in loginActivity codes

--- app/src/main/java/com/example/umonlineappointmentsystem/LoginActivity.java
+++ app/src/main/java/com/example/umonlineappointmentsystem/LoginActivity.java
@@ -1,23 +1,63 @@
 package com.example.umonlineappointmentsystem;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.WindowManager;
 import android.widget.Button;
+import android.widget.Toast;
 
-public class LoginActivity extends AppCompatActivity {
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.auth.api.signin.GoogleSignInResult;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+
+public class LoginActivity extends AppCompatActivity implements GoogleApiClient.OnConnectionFailedListener{
+
+    private GoogleApiClient googleApiClient;
+    private static final int SIGN_IN = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_login);
+
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN).requestEmail().build();
+
+        googleApiClient = new GoogleApiClient.Builder(this).enableAutoManage(this, this)
+                .addApi(Auth.GOOGLE_SIGN_IN_API, gso).build();
+
         Button button = (Button) findViewById(R.id.button);
         button.setOnClickListener(view -> {
-            Intent i = new Intent(this, DashboardActivity.class);
-            startActivity(i);
+            Intent i = Auth.GoogleSignInApi.getSignInIntent(googleApiClient);
+            startActivityForResult(i, SIGN_IN);
         });
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(requestCode == SIGN_IN){
+            GoogleSignInResult result = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
+
+            if(result.isSuccess()){
+                startActivity(new Intent(LoginActivity.this, DashboardActivity.class));
+                finish();
+            }else{
+                Toast.makeText(this, "Login failed!", Toast.LENGTH_SHORT).show();
+            }
+        }
+
     }
 }
